a75a30e3de82575eec814b486a35497b2b207736
Villy Siu <villysiu@gmail.com>
2024-01-12 16:13:54 -0800

    media query single wine

--- src/features/wine/SIngleWine.js
+++ src/features/wine/SIngleWine.js
@@ -12,7 +12,48 @@ const SingleWine = () =>{
     })
 
     const singleWine = wine_arr.find(wine => wine.pk === parseInt(itemId))
+    const title = singleWine.year +" " + singleWine.title + " very very long name"
+    const Desc = () =>{
+        return (
+            <>
+                {singleWine.description}
+                {/* {singleWine.description}
+                {singleWine.description}
+                {singleWine.description}
+                {singleWine.description}
+                {singleWine.description}
 
+                {singleWine.description}
+                {singleWine.description} */}
+            </>
+        )
+    }
+    const Props = () =>{
+        return (
+            <ul className='singlewine_props_wrapper'>
+                            <li className='singlewine_prop pe-3'>
+                                <span>WINEMAKER</span>
+                                <span>Danielle</span>
+                            </li>
+                            <li className='singlewine_prop ps-3 pe-3'>
+                                <span>APPELLATION</span>
+                                <span>{singleWine.origin}</span>
+                            </li>
+                            <li className='singlewine_prop ps-3' style={{borderRight: '0px'}}>
+                                <span>VARIETAL</span>
+                                <span>{singleWine.varietal}</span>
+                            </li>
+                        </ul>
+        )
+    }
+    const Price =() =>{
+        return(
+            <div className='singlewine_cart mb-3'>
+                <span className="pe-4">{USDollar.format(singleWine.price)}</span>
+                <PurchaseButton menuitemId={singleWine.pk} menuitemTitle={singleWine.year +" " + singleWine.title} price={singleWine.price} />
+            </div>
+        )
+    }
     if(singleWine===undefined)
         return( <div>cannot find wine, not existed?</div>)
         let myDivObj = document.getElementById("singlewine_bg"); 
@@ -22,82 +63,48 @@ const SingleWine = () =>{
         <div className="singlewine_wrapper">
             <div className="singlewine_bg_wrapper" >
                 <img src={`${homeLink}/pick_grape_bg.jpeg`} id="singlewine_bg" alt={singleWine.title}></img>
-                <div className="singlewine_img_title_wrapper app_width">
+                
+                <div className="singlewine_img_pos_wrapper app_width d-none d-sm-block">
                     <div className="singlewine_img_wrapper">
                         <img src={`${homeLink}/ASC_websize.png`} className="singlewine_img" alt={singleWine.title}></img>
                     </div>
+                </div>
+                <div className="singlewine_title_pos_wrapper app_width d-none d-sm-block">
                     <div className="singlewine_title_cart_props_wrapper">
                         <div className='singlewine_title '> 
-                            {singleWine.year} {singleWine.title}
+                            {title}
                         </div>
-                        <div className='singlewine_cart mb-3'>
+                       <Price />
+                        {/* <div className='singlewine_cart mb-3'>
                             <span className="pe-4">{USDollar.format(singleWine.price)}</span>
                             <PurchaseButton menuitemId={singleWine.pk} menuitemTitle={singleWine.year +" " + singleWine.title} price={singleWine.price} />
+                        </div> */}
+                        <Props />
+                        
+                    </div>
+                </div>
+                <div className=" d-block d-sm-none singlewine_title_img_visibility_wrapper">
+                    <div className="singlewine_title_img_sm_wrapper">
+                        <div className='singlewine_title '> 
+                            {title}
                         </div>
-                        <ul className='singlewine_props_wrapper'>
-                            <li className='singlewine_prop pe-3'>
-                                <span>WINEMAKER</span>
-                                <span>Danielle</span>
-                            </li>
-                            <li className='singlewine_prop ps-3 pe-3'>
-                                <span>APPELLATION</span>
-                                <span>{singleWine.origin}</span>
-                            </li>
-                            <li className='singlewine_prop ps-3' style={{borderRight: '0px'}}>
-                                <span>VARIETAL</span>
-                                <span>{singleWine.varietal}</span>
-                            </li>
-                        </ul>
+                        
+                        <div className="singlewine_img_wrapper">
+                            <img src={`${homeLink}/ASC_websize.png`} className="singlewine_img" alt={singleWine.title}></img>
+                        </div>
+                         
                     </div>
                 </div>
             </div>
-            <div className="singlewine_desc_wrapper app_width">
-                {singleWine.description}
-                {singleWine.description}
-                {singleWine.description}
-                {singleWine.description}
-                {singleWine.description}
-                {singleWine.description}
-
-                {singleWine.description}
-                {singleWine.description}
+            <div className="d-block d-sm-none" style={{"margin-top": "8rem"}}>
+                <Price />
+                <Props />
             </div>
-
-            {/* <div className='singlewine_part_wrapper'> */}
-                {/* <div className='singlewine_img_wrapper-1'> */}
-                    {/* <div className="singlewine_img_wrapper">
-                        <img src={`${homeLink}/ASC_websize.png`} className="singlewine_img" alt={singleWine.title}></img>
-                    </div> */}
-                    
-                {/* </div> */}
-                {/* <div className='singlewine_info_wrapper'>
-                    <div className="www">
-                        <div className='singlewine_title '> 
-                            {singleWine.year} {singleWine.title}
-                        </div>
-                        <div className='singlewine_cart mb-3'>
-                            <span className="pe-4">{USDollar.format(singleWine.price)}</span>
-                            <PurchaseButton menuitemId={singleWine.pk} menuitemTitle={singleWine.year +" " + singleWine.title} price={singleWine.price} />
-                        </div>
-                        <ul className='singlewine_props_wrapper'>
-                            <li className='singlewine_prop pe-3'>
-                                <span>WINEMAKER</span>
-                                <span>Danielle</span>
-                            </li>
-                            <li className='singlewine_prop ps-3 pe-3'>
-                                <span>APPELLATION</span>
-                                <span>{singleWine.origin}</span>
-                            </li>
-                            <li className='singlewine_prop ps-3' style={{borderRight: '0px'}}>
-                                <span>VARIETAL</span>
-                                <span>{singleWine.varietal}</span>
-                            </li>
-                        </ul>
-                    </div>
-
-                </div> */}
-            {/* </div>  */}
+            <div className="singlewine_desc_wrapper app_width ">
+                <Desc />
+            </div>
             
+
          </div>
 
     )
