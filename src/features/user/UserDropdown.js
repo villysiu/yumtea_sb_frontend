fae67bcd1de38e84ccf32e25a01512721a790155
Villy Siu <villysiu@gmail.com>
2024-01-18 12:15:02 -0800

    adjust z-index layer and position of user dropdown

--- src/features/user/UserDropdown.js
+++ src/features/user/UserDropdown.js
@@ -3,7 +3,7 @@ import AccountButton from "./AccountButton"
 const UserDropdown = () =>{
     return(
         <div className='user_dropdown_wrapper'>
-            <div className='user_dropdown mt-1 pt-3'>
+            <div className='user_dropdown mt-3 pt-3'>
                 <AccountButton />
                 <div className='pb-2'>Order History</div>
                 <div>Reservations</div>
