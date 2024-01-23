a3d436f4faf8246bbaddaebc216fb7347d884537
Villy Siu <villysiu@gmail.com>
2024-01-23 14:52:41 -0800

    setup order details filter

---
+++ src/features/order/OrderFilter.js
@@ -0,0 +1,43 @@
+import Dropdown from 'react-bootstrap/Dropdown';
+import { Check } from 'react-bootstrap-icons';
+
+const OrderFilter = ({selected, setSelected}) =>{
+    
+    
+    
+    const filterChoices = [
+        "All Order",
+        "Past 3 Months",
+        "Current Year",
+        "Last Year"
+    ]
+    
+
+    
+   return (
+    <Dropdown className='order_filter_container'>
+        <div className='order_filter_title'>Order Date</div>
+        <Dropdown.Toggle className='order_filter_box' >
+            <div>{filterChoices[selected]} </div>
+            
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu className='order_filter_choices'>
+        {
+            filterChoices.map((choice, idx)=>{
+                return (
+                    <Dropdown.Item className='order_filter_choice' onClick={()=>setSelected(idx)} >
+                        <div style={{width: '1.5rem'}}>
+                            {selected === idx && <Check />}
+                        </div>
+                        <div>{choice}</div>
+                    </Dropdown.Item>
+                )
+            })
+        }
+        </Dropdown.Menu>
+    </Dropdown>
+   )
+    
+}
+export default OrderFilter
--- src/features/order/OrderHistory.js
+++ src/features/order/OrderHistory.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { fetchCurrentUserOrders } from "./orderSlice"
 import SingleOrder from "./SingleOrder"
+import OrderFilter from "./OrderFilter"
 
 const OrderHistory = () =>{
 
@@ -9,27 +10,63 @@ const OrderHistory = () =>{
     const {status, orders_arr} = useSelector(state => state.order.order) 
 
     const [show, setShow] = useState(null)
-    
+    const [selected, setSelected] =useState(0)
     
 
     useEffect(()=>{
         if(status === 'idle')
             dispatch(fetchCurrentUserOrders())
     }, [])
+
+    const DateFilterHelper = (date) =>{
+        
+        if(selected===1){
+            const current = new Date()
+            current.setDate(current.getDate()-30)
+    
+            return new Date(date) > current
+        
+        }
+        if(selected === 2){
+            const current = new Date().getFullYear()
+            const regex = /(\d{4})/g;
+            const found = date.match(regex);
+            
+            return parseInt(found[0]) === current
+        }
+        if(selected === 3){
+            const last = new Date().getFullYear() -1
+            const regex = /(\d{4})/g;
+            const found = date.match(regex);
+            
+            return parseInt(found[0]) === last
+        }
+        return true
+    }
     return (
         <div className='app_width'>
             <h2 style={{textAlign: 'center'}}>Order History</h2>
             
             <div className="orderhistory_wrapper">
-            <div style={{width: '100%', textAlign: 'right'}}>Order Date filter dropbox goes here</div>
-            {
-                orders_arr.map(order => {
-                    return (
-                        <SingleOrder key={order.pk} order={order} show={show} setShow={setShow}/>
+                
 
-                    )
-                })
-            }
+                <div style={{width: '100%', textAlign: 'right'}}>
+                    <OrderFilter selected={selected} setSelected={setSelected} />
+                </div>
+                {
+                    orders_arr
+                    .filter(order=>{
+                        return (
+                            DateFilterHelper(order.date)
+                        )
+                    })
+                    .map(order => {
+                        return (
+                            <SingleOrder key={order.pk} order={order} show={show} setShow={setShow}/>
+
+                        )
+                    })
+                }
             </div>
         </div>
 
