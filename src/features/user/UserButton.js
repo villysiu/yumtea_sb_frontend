8656fd075eaac8f7e915e17bcb642467b77b2b88
Villy Siu <villysiu@gmail.com>
2024-01-23 20:10:38 -0800

    Updated user account button

--- src/features/user/UserButton.js
+++ src/features/user/UserButton.js
@@ -2,17 +2,16 @@ import { useState } from "react"
 import { useSelector } from "react-redux"
 import { homeLink } from "../../app/global"
 import { Link } from 'react-router-dom';
-import { PersonCircle } from "react-bootstrap-icons";
+import { PersonCircle, PersonFill } from "react-bootstrap-icons";
 import UserDropdown from "./UserDropdown";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { fetchCurrentUser } from "./userSlice";
+import { Spinner } from "react-bootstrap";
 const UserButton = ({showMDFullscrenn, setShowMDFullscrenn}) =>{
     const dispatch=useDispatch();
     const current_user = useSelector(state => state.user.current_user )
     const [show, setShow] = useState(false)
-    
-    
     
     useEffect(()=>{
       console.log("user login I am in APP.ks")
@@ -31,32 +30,25 @@ const UserButton = ({showMDFullscrenn, setShowMDFullscrenn}) =>{
         
         return (
             <>
-                {/* max-width: 992px */}
-                <div className='d-lg-none '>
-                    <Link to={`${homeLink}/secure/account`} className="solid_link" 
-                    onClick={()=>setShowMDFullscrenn(false)}>
-                        My Account
-                    </Link>
-                </div>
-                {/* min-width: 992px */}
-                <div className='d-none d-lg-block'>
+                {
+                    !showMDFullscrenn && 
                     <div className="hello_user" 
                         onMouseEnter={handleMouseenter}
                         onMouseLeave={handleMouseleave}
                     >
                         Hello {current_user.username}
+                        <PersonFill className='circle_button' />
                         {
                             show && <UserDropdown />
                         }
-                
                     </div>
-                </div>
+                }
             </>
         )
       
     }
     if(current_user.status === 'loading')
-        return <div>Loading</div>
+        return <Spinner />
 
     return (
         <>
