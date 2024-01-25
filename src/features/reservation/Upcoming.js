5228bd5be0694ebd1880ee76e7b5b7f4d6fe6cd5
Villy Siu <villysiu@gmail.com>
2024-01-25 14:39:46 -0800

    setup Upcoming reservations display clock

---
+++ src/features/reservation/Upcoming.js
@@ -0,0 +1,56 @@
+import { fetchReservations } from "./reservationSlice"
+import { useDispatch } from "react-redux"
+import { useSelector } from "react-redux"
+import { useEffect } from "react"
+import { Spinner } from "react-bootstrap"
+import { Clock } from "react-bootstrap-icons"
+import { Calendar3 } from "react-bootstrap-icons"
+import { PeopleFill } from "react-bootstrap-icons"
+import { PencilSquare } from "react-bootstrap-icons"
+const Upcoming = () =>{
+    const dispatch = useDispatch()
+    const reservation = useSelector(state=>state.reservation.upcoming_reservations)
+    useEffect(()=>{
+        if(reservation.status==='idle'){
+            dispatch(fetchReservations())
+        }
+    }, [dispatch, reservation.status])
+
+    if(reservation.status === 'loading')
+        return <Spinner />
+
+    if(reservation.upcoming_reservations_arr.length===0)
+        return <div>No upcoming res at all, make one now</div>
+
+    return (
+        <div className='upcoming_reservation_container mt-5'>
+            <div className='upcoming_reservation_title'>Upcoming reservation</div>
+            {
+                reservation.upcoming_reservations_arr.map(reservation=>{
+                    return (
+                        <div className='single_upcoming_reservation_container'>
+                            
+                            <div className='upcoming_reservation_date'>
+                                <Calendar3 className='me-2'/>{reservation.reservation_date}
+                            </div>
+                        
+                            <div className='upcoming_reservation_time'>
+                                <Clock className='me-2'/>{reservation.reservation_time}
+                            </div>
+                        
+                            <div className='upcoming_reservation_guests'>
+                                <PeopleFill className='me-2'/>{reservation.no_of_guests}
+                            </div>
+                            <div className='upcoming_reservation_edit_button'>
+                                <PencilSquare  className='me-2'/>
+                            </div>    
+                            
+                           
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
+export default Upcoming
