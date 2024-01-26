535782f6e206f7a8929e063f83bfb81339e816ac
Villy Siu <villysiu@gmail.com>
2024-01-26 09:41:45 -0800

    added restriction to direct access

--- src/features/reservation/ReservationSuccess.js
+++ src/features/reservation/ReservationSuccess.js
@@ -1,34 +1,50 @@
 import { useLocation } from "react-router-dom";
-import { Navigate } from "react-router-dom";
 import { Button } from "react-bootstrap";
+import { homeLink } from "../../app/global";
+import { Link } from "react-router-dom";
+import { Clock } from "react-bootstrap-icons"
+import { Calendar3 } from "react-bootstrap-icons"
+import { PeopleFill } from "react-bootstrap-icons"
 const ReservationSuccess = () =>{
-    let location = useLocation();
-    console.log(location)
+    let {state} = useLocation();
+    console.log(state)
     // prohibited direct access this page
    
     // if(!location.state || state.from !== '/secure/reservation')
     //     return <Navigate to="../../" replace={true} />
 
-    let state={
-        data: {pk: 41, user_id: 2, no_of_guests: 2, reservation_date: '2024-01-25', reservation_time: '15:30:00'},
-
-    }
-    const reservation = state.data
+    
+    const {reservation} = state
     return (
-        <div className='reservation_success_container'>Your reservaiotn is confirmed. 
-            <div className="reservation_success_date">
-                <div className="reservation_success_left_column">Date: </div>
-                <div>{reservation.reservation_date}</div>
+        <div className="reserve_wrapper">
+            <div className="reserve_bg_wrapper">
+                <img src={`${homeLink}/2018-CK-lifestyle.jpeg`} alt="" className="singlewine_bg"></img>
+            
+                <div className='reserve_title_container'>
+                    <div className='reserve_title'><b>Little D Tasting Reservations </b></div>
+                </div>
             </div>
-            <div className="reservation_success_date">
-                <div className="reservation_success_left_column">Time: </div>
-                <div>{reservation.reservation_time}</div>
+            <div className='reservation_success_container'>
+                <div className='reservation_success_title mb-3'>Your reservaiotn is confirmed.</div>
+                <div key={reservation.pk} className='reservation_success_details_container'>
+                                
+                    <div className='reservation_success_date'>
+                        <Calendar3 className='me-2'/>{reservation.reservation_date}
+                    </div>
+                
+                    <div className='reservation_success_time'>
+                        <Clock className='me-2'/>{reservation.reservation_time.slice(0,5)}
+                    </div>
+                
+                    <div className='reservation_success_guests'>
+                        <PeopleFill className='me-2'/>{reservation.no_of_guests}
+                    </div>
+                </div>
+                <Link to={`${homeLink}/secure/reservations`} className=''>
+                    <Button className='gold_button'>Back to reservations</Button>
+                </Link>
             </div>
-            <div className="reservation_success_date">
-                <div className="reservation_success_left_column">Guests: </div>
-                <div>{reservation.no_of_guests}</div>
-            </div>
-            <Button className='gold_button'>See your reservations</Button>
+            
         </div>
     )
 }
---
+++ src/features/reservation/UpdateReservation.js
@@ -0,0 +1,67 @@
+import { Form } from "react-bootstrap"
+import { homeLink } from "../../app/global"
+
+import { useState } from "react"
+
+import { useDispatch } from "react-redux"
+import { updateReservation } from "./reservationSlice"
+import { useLocation, useNavigate } from "react-router-dom"
+import ReserveForm from "./ReserveForm"
+// import { useParams } from "react-router-dom"
+// import { useSelector } from "react-redux"
+// import { getReservationById } from "./reservationSlice"
+
+const UpdateReservation =() =>{
+
+    //  setup no direct access later
+    // const {pk} = useParams()
+    const {state} = useLocation()
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const reservation = state.reservation
+    console.log(reservation)
+    
+
+    const [date, setDate] = useState(reservation.reservation_date)
+    const [time, setTime] = useState(reservation.reservation_time.slice(0,5))
+    const [guest, setGuest] = useState(reservation.no_of_guests)
+    
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        
+        const formData={
+            'reservation_date': date,
+            'reservation_time': `${time}:00`,
+            'no_of_guests': guest
+        }
+        console.log(formData)
+        dispatch(updateReservation({pk:reservation.pk, data: formData}))
+        .unwrap()
+        .then((originalPromiseResult) => {
+            console.log(originalPromiseResult)
+            console.log('hhh?')
+            navigate("../../reservation/success", { state: { reservation: originalPromiseResult} });
+        })
+        .catch((rejectedValueOrSerializedError) => {
+        // handle error here
+        })
+    }
+    return(
+       
+        <div className="reserve_wrapper">
+            <div className="reserve_bg_wrapper">
+                <img src={`${homeLink}/2018-CK-lifestyle.jpeg`} alt="" className="singlewine_bg"></img>
+            
+                <div className='reserve_title_container'>
+                    <div className='reserve_title'><b>Little D Tasting Reservations </b></div>
+                </div>
+            </div>
+            <Form onSubmit={handleSubmit} className='reserve_container'>
+                <ReserveForm date={date} setDate={setDate} time={time} setTime={setTime} 
+                    guest={guest} setGuest={setGuest} btnName={'Update Reservation'} />
+            </Form>
+        </div>
+    )
+}
+export default UpdateReservation
