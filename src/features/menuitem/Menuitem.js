ba7ae1af3da171d2f007b361dfecc2f9f3231a0b
Villy Siu <villysiu@gmail.com>
2024-01-30 21:28:23 -0800

    show popup on click

--- src/features/menuitem/Menuitem.js
+++ src/features/menuitem/Menuitem.js
@@ -1,32 +1,46 @@
 import { Col } from "react-bootstrap"
+import { Modal } from "react-bootstrap"
 import { Link } from "react-router-dom"
 import { homeLink } from "../../app/global"
 import { USDollar } from "../../app/global"
-import PurchaseButton from "./PurchaseButton"
 import OutOfStockButton from "./OutOfStockButton"
-const Wine = ({menuitem}) =>{
-    
-    const title = menuitem.year +" " + menuitem.title
+import { useState } from "react"
+import CustomizeList from "./CustomizeList"
+import { Button } from "react-bootstrap"
+import AddedOverlay from "./AddedOverlay"
+const Menuitem = ({menuitem}) =>{
+
+    const [show, setShow] = useState(false);
+    const [message, setMessage] = useState("") 
     return (
+        <>
+        {
+            <Modal show={show} onHide={()=>setShow(false)}>
+                <CustomizeList singleMenuitem={menuitem} setShow={setShow} setMessage={setMessage} /> 
+            </Modal>
+        }
+        
         <Col sm={12} md={4} lg={3} className="winelist_wine_wrapper"> 
             <Link to={`${homeLink}/menuitems/${menuitem.pk}`} className='solid_link'>
                 <div className="winelist_img_wrapper">
                     <img src={`${homeLink}/IMG_0210.png`} className="winelist_img" alt={menuitem.title}></img>  
                 </div>      
-                <div className="winelist_wine_title">{menuitem.year} {menuitem.title}</div>
+                <div className="winelist_wine_title">{menuitem.title}</div>
             </Link>
        
-            <div>{menuitem.varietal}</div>
+
             <div className='mb-3'>{USDollar.format(menuitem.price)}</div>
 
             { menuitem.inventory === 0 ?
                 <OutOfStockButton />
                 :
-
-                <PurchaseButton menuitemId={menuitem.pk} menuitemTitle={title} price={menuitem.price}/>
+                <div style={{"position": "relative"}}>
+                    <Button className='gold_button short' onClick={()=>setShow(true)}>Customize</Button>
+                    {message && <AddedOverlay message={message} setMessage={setMessage}/>}
+                </div>
             }
         </Col>
-
+        </>
     )
 }
-export default Wine
+export default Menuitem
