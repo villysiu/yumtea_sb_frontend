46b76330f9d5bee154aa02750b2a7791746aeadc
Villy Siu <villysiu@gmail.com>
2025-02-21 12:37:58 -0800

    setup bestseller section

--- src/features/menuitem/BestSellers.js
+++ src/features/menuitem/BestSellers.js
@@ -1,7 +1,23 @@
+import {useSelector} from "react-redux";
+import {getMenuitemsByCategoryId} from "./menuitemSlice";
+import {homeLink} from "../../app/global";
+import {Col, Row} from "react-bootstrap";
+import Menuitem from "./Menuitem";
+
 const BestSellers = () =>{
-    return(
-        <div className='bestsellers_wrapper'>
-            Best Sellers
+   const bestsellers = []
+
+    return (
+        <div className='category'>
+            <div className='category_wrapper'>
+                <img src={`${homeLink}/category/12typestea.jpg`}
+                     className="category_img" alt="Best Sellers"
+                />
+                <div className='category_label'>
+                    Best Sellers
+                </div>
+
+            </div>
         </div>
     )
 }
