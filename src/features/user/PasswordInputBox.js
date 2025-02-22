52596969a0b8460ce4c9392ab9adaa2d236616ef
Villy Siu <villysiu@gmail.com>
2025-02-22 09:12:12 -0800

    reversed eye

---
+++ src/features/user/PasswordInputBox.js
@@ -0,0 +1,50 @@
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
+import {useState} from "react";
+import InputGroup from "react-bootstrap/esm/InputGroup";
+import {Eye, EyeSlash} from "react-bootstrap-icons";
+
+const PasswordInputBox = ({password, setPassword}) => {
+    const [passwordError, setPasswordError] = useState("")
+    const [showPassword, toggleShowPassword] = useState(false)
+
+    const handleChange = (e) => {
+        setPassword(e.target.value)
+        if(passwordError)
+            validatePassword(e.target.value)
+    }
+    const validatePassword = (pa) =>{
+        console.log("validate password")
+        const re = /(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,20}/
+        if(pa === "")
+            setPasswordError("Password is required")
+        else if(!re.test(pa)) {
+            setPasswordError("Password must be 8-20 characters long, include at least one letter and one number")
+        }
+        else
+            setPasswordError("")
+
+    }
+    return (
+        <>
+            <FloatingLabel controlId="floatingInput" label="Password"
+                className={passwordError? "password_eye_box" : "password_eye_box mb-3"}
+
+            >
+
+                <Form.Control type={showPassword ? "text" : "password"} className="password_input"
+                              placeholder="Password"
+                              value={password}
+                              onChange={handleChange}
+                              onBlur={e=>validatePassword(e.target.value)}
+                />
+                <InputGroup.Text onClick={()=>toggleShowPassword(!showPassword)} className="password_eye">
+                    {showPassword ? <EyeSlash /> : <Eye />}
+                </InputGroup.Text>
+            </FloatingLabel>
+            {passwordError && <p className="inpurbox_error">{passwordError}.</p>}
+        </>
+
+    )
+}
+export default PasswordInputBox
