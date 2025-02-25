ac4ed395f29755a4175a68791dbb235b1b50cf42
Villy Siu <villysiu@gmail.com>
2025-02-24 17:09:48 -0800

    setshow answer to false when clicked outside of ref

--- src/features/support/QnA.js
+++ src/features/support/QnA.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useEffect, useRef, useState} from "react";
 
 const QnA = () =>{
 
@@ -17,14 +17,30 @@ const QnA = () =>{
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."]
 
     ]
+
+    const ref = useRef();
     const handleClick = (idx) =>{
         if(show === null || show!==idx)
             setShow(idx);
         else if(show === idx)
             setShow(null);
     }
+    useEffect(()=>{
+        const closeAns = (e) =>{
+            if(ref && ref.current && !ref.current.contains(e.target))
+                setShow(null)
+        }
+
+        document.addEventListener('click', closeAns)
+
+        return () => {
+            document.removeEventListener('click', closeAns);
+        };
+    }, [ref])
     return(
-        <div className="qnas">
+        <>
+            <div className="support_title">Support</div>
+        <div ref={ref} className="qnas mb-4" >
             {arr.map( (qqq,idx)=>{
                 const [q,a] = qqq
                 return (
@@ -37,6 +53,7 @@ const QnA = () =>{
 
             })}
         </div>
+    </>
     )
 
 
