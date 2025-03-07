1a6153eba68d981aac47dd24e69f31bb983b1ef5
Villy Siu <villysiu@gmail.com>
2025-03-07 08:13:32 -0800

    added bestsellers flags

--- src/features/menuitem/SingleBestSeller.js
+++ src/features/menuitem/SingleBestSeller.js
@@ -2,12 +2,16 @@ import {useSelector} from "react-redux";
 import {getMenuitemById} from "./menuitemSlice";
 import Menuitem from "./Menuitem";
 import {Col} from "react-bootstrap";
+import {StarFill, BookmarkFill} from "react-bootstrap-icons";
 
 const SingleBestSeller = ({menuitemId}) =>{
     const menuitem = useSelector(state=>getMenuitemById(state, menuitemId))
     return (
-        <Col xs={12} md={4} className="mb-3" key={menuitem.id}>
+        <Col xs={12} md={4} className="mb-3" key={menuitem.id} style={{"position": "relative"}}>
+
             <Menuitem menuitem={menuitem}/>
+            <StarFill className="yellowbookmark" />
+            3
         </Col>
 
     )
